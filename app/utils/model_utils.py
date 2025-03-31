494c61356d8c23986f95d3585190b2ef18304c79
Jonathan Garcia <garciajonathan268@gmail.com>
2025-03-30 22:53:14 -0500

    ✨ Add new audio files and models for appointment type and modality detection, update CSV and JSON data structures, and refactor model training functions.

--- app/utils/model_utils.py
+++ app/utils/model_utils.py
@@ -5,22 +5,38 @@ from sklearn.pipeline import Pipeline
 import joblib
 
 # Entrenamiento del modelo con datos del CSV
-def entrenar_modelo(ruta_csv, ruta_modelo):
+def entrenar_modelo_tipo_cita(ruta_csv, ruta_modelo):
     df = pd.read_csv(ruta_csv)
     X = df["texto"]
-    y = df["opcion"]
-
+    y = df["tipo_cita"]
+    
     pipeline = Pipeline([
         ('tfidf', TfidfVectorizer()),
         ('clf', DecisionTreeClassifier())
     ])
-
+    
     pipeline.fit(X, y)
     joblib.dump(pipeline, ruta_modelo)
-    print("✅ Modelo entrenado y guardado:", ruta_modelo)
+    print("✅ Modelo de tipo de cita entrenado:", ruta_modelo)
 
-# Cargar el modelo y predecir la categoría a partir del texto
-def predecir_categoria(texto, ruta_modelo):
+# modelo para modalidad
+def entrenar_modelo_modalidad(ruta_csv, ruta_modelo):
+    df = pd.read_csv(ruta_csv)
+    X = df["texto"]
+    y = df["modalidad"]
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', DecisionTreeClassifier())
+    ])
+    pipeline.fit(X, y)
+    joblib.dump(pipeline, ruta_modelo)
+    print("✅ Modelo de modalidad entrenado:", ruta_modelo)
+
+def predecir_modalidad(texto, ruta_modelo):
     modelo = joblib.load(ruta_modelo)
-    prediccion = modelo.predict([texto])
-    return prediccion[0]
+    return modelo.predict([texto])[0]
+
+
+def predecir_tipo_cita(texto, ruta_modelo):
+    modelo = joblib.load(ruta_modelo)
+    return modelo.predict([texto])[0]
